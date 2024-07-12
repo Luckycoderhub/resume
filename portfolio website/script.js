aae26397a17149345a90060f8a1b3d2f7e7cd9a2
Luckycoderhub <164616726+Luckycoderhub@users.noreply.github.com>
2024-07-12 13:17:14 +0530

    Add files via upload

---
+++ portfolio website/script.js
@@ -0,0 +1,99 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+const blurlHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') :
+        header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurlHeader)
+
+// ========================Email Js======================
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+
+    e.preventDefault();
+
+    emailjs.sendForm('service_0lbytmv', 'template_behwuty', '#contact-form', 'v6wHhFJbRqNNtoPkW')
+        .then(() => {
+
+
+            contactMessage.textContent = 'Message sent successfully ✅'
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+            contactForm.reset()
+        }, () => {
+            contactMessage.textContent = 'Messaage not sent (service error)❌'
+        })
+}
+contactForm.addEventListener('submit', sendEmail)
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') :
+        scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+sr.reveal('.home__data ,.experience ,.skills, .contact__container')
+sr.reveal('.home__img', { delay: 600 })
+sr.reveal('.home__scroll', { delay: 900 })
+sr.reveal('.work__card , .services__card', { interval: 100 })
+sr.reveal('.about__content', { origin: 'right' })
+sr.reveal('.about__img', { origin: 'left' })
